71b04a82b965b23821df0a61001670cc9092af5f
강희연 <the.onlyeon@gmail.com>
2020-02-17 08:46:35 +0900

    3주차 수업

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
-// 문서 객체
-const nav = document.querySelector('.appNavigation')
+// 1. 동작에 대해 설계한 후, 객체를 선택하는 작업
+const nav = document.querySelector('.menu')
+// 3line에서 불러온 menu에서 탐색하도록 작성
 const btn = nav.querySelector('.buttonBurger')
+// querySelectorAll 여러개의 item을 모두 선택해주도록 작성
 const items = nav.querySelectorAll('.menu__item')
 const links = nav.querySelectorAll('.menu__link')
 
 // 상태 변수
+// 2. 변수를 만들고 초기값을 선언함
 let mode = null
 let isPristine = true
-
-// 렌더 함수
-function render() {
-  // 모바일 디바이스 감지 (조건)
+function render(){
+  // window의 inner width값이 999보다 작거나 같을 때
   let isMobile = window.innerWidth <= 999
-  // 리사이즈 이벤트가 여러 번 발생해도 모드가 변경되지 않으면 함수 실행 중지
   if (mode === isMobile) {
     return
   }
-  // 모드가 변경되면, 모드의 상태 업데이트
   mode = isMobile
-
   if (isMobile) {
     // 모바일 환경의 경우....
     btn.setAttribute('aria-haspopup', 'true')
@@ -39,14 +37,14 @@ function render() {
       // 버튼(btn)을 클릭하면 내비게이션(nav) 요소를 찾아서
       // isAct라는 클래스를 추가하거나 제거 할 것(toggle)
       btn.addEventListener('click', function (e) {
-        if (nav.classList.contains('isAct')) {
-          btn.setAttribute('aria-label', '메뉴 열기')
-          btn.setAttribute('aria-pressed', 'false')
-        } else {
+        if (nav.classList.contains('menu--isAct')) {
           btn.setAttribute('aria-label', '메뉴 닫기')
           btn.setAttribute('aria-pressed', 'true')
+        } else {
+          btn.setAttribute('aria-label', '메뉴 열기')
+          btn.setAttribute('aria-pressed', 'false')
         }
-        nav.classList.toggle('isAct')
+        nav.classList.toggle('menu--isAct')
       })
       // 메뉴 버튼(.menu-item)을 클릭하면
       // 클릭한 버튼의 부모 요소의 형제 요소들을 찾아 menu-act라는 클래스를 삭제한다.
@@ -61,7 +59,7 @@ function render() {
 
           for (var i = 0; i < items.length; i++) {
             var item = items[i]
-            item.classList.remove('menuAct')
+            item.classList.remove('menu__item--isAct')
             item.classList.remove('icon-minus')
             item.classList.add('icon-plus')
           }
@@ -72,11 +70,11 @@ function render() {
             link.setAttribute('aria-expanded', 'false')
           }
 
-          _this.parentNode.classList.add('menuAct')
+          _this.parentNode.classList.add('menu__item--isAct')
           _this.setAttribute('aria-pressed', 'true')
           _this.setAttribute('aria-expanded', 'true')
 
-          if (_parent.classList.contains('menuAct')) {
+          if (_parent.classList.contains('menu__item--isAct')) {
             _parent.classList.remove('icon-plus')
             _parent.classList.add('icon-minus')
           }
@@ -92,17 +90,17 @@ function render() {
       let item = items[i]
       item.classList.remove('icon-plus')
       item.classList.remove('icon-minus')
-      item.classList.add('icon-star')
     }
     for (let i = 0; i < links.length; i++) {
       let link = links[i]
       link.setAttribute('role', 'presentation')
-      // 포커스를 받을 필요가 없기 때문에 tab-index="-1" 로 만들어주는 코드가 필요하다
+      // 포커스를 받을 필요가 없기 때문에 tabindex="-1" 로 만들어주는 코드가 필요하다
+      link.setAttribute('tabindex', '-1')
     }
   }
-
 }
 
 // 이벤트 연결 [로드, 리사이즈]
 window.addEventListener('load', render)
 window.addEventListener('resize', render)
+
